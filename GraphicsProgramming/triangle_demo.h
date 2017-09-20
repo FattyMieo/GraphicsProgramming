d55f322746888edcb2768c91b281c62a1fd08da0
FattyMieo <fong2009@live.com.my>
2017-09-20 18:11:30 +0800

    Waving Orbiter

--- GraphicsProgramming/triangle_demo.h
+++ GraphicsProgramming/triangle_demo.h
@@ -68,7 +68,7 @@ public:
 		glBegin(GL_TRIANGLES);
 
 		// Blue
-		glColor3f(0.0f, 0.0f, 1.0f);
+		glColor3f(0.25f, 0.25f, 1.0f);
 
 		// Negative Square
 		glVertex3f(-0.5f, 0.5f, -0.5f);
@@ -89,7 +89,7 @@ public:
 		glVertex3f(0.5f, -0.5f, 0.5f);
 
 		// Green
-		glColor3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.25f, 1.0f, 0.25f);
 
 		// Negative Square
 		glVertex3f(0.5f, -0.5f, -0.5f);
@@ -110,7 +110,7 @@ public:
 		glVertex3f(-0.5f, 0.5f, 0.5f);
 
 		// Red
-		glColor3f(1.0f, 0.0f, 0.0f);
+		glColor3f(1.0f, 0.25f, 0.25f);
 
 		// Negative Square
 		glVertex3f(-0.5f, -0.5f, 0.5f);
@@ -138,7 +138,7 @@ public:
 		glBegin(GL_TRIANGLES);							// Drawing Using Triangles
 
 		// Base
-		glColor3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.25f, 1.0f, 0.25f);
 		glVertex3f(0.5f, -0.5f, 0.5f);					// Top
 		glVertex3f(0.5f, -0.5f, -0.5f);					// Bottom Left
 		glVertex3f(-0.5f, -0.5f, 0.5f);					// Bottom Right
@@ -148,7 +148,7 @@ public:
 		glVertex3f(-0.5f, -0.5f, 0.5f);					// Bottom Right
 
 		// Red Triangles
-		glColor3f(1.0f, 0.0f, 0.0f);
+		glColor3f(1.0f, 0.25f, 0.25f);
 		glVertex3f(0.0f, 0.5f, 0.0f);					// Top Vertex
 		glVertex3f(0.5f, -0.5f, -0.5f);					// Bottom Left
 		glVertex3f(0.5f, -0.5f, 0.5f);					// Bottom Right
@@ -158,7 +158,7 @@ public:
 		glVertex3f(-0.5f, -0.5f, 0.5f);					// Bottom Right
 
 		// Blue Triangles
-		glColor3f(0.0f, 0.0f, 1.0f);
+		glColor3f(0.25f, 0.25f, 1.0f);
 		glVertex3f(0.0f, 0.5f, 0.0f);					// Top Vertex
 		glVertex3f(-0.5f, -0.5f, 0.5f);					// Bottom Left
 		glVertex3f(0.5f, -0.5f, 0.5f);					// Bottom Right
@@ -192,6 +192,7 @@ public:
 			float z2 = radius * sin(((i + 1) * t) * PI / 180.0);
 
 			// ---- Upper Circle ----
+			glColor3f(1.0f, (i % 2 == 0 ? 0.5f : 1.0f), 0.5f);
 			//Center of the circle
 			glVertex3f(offsetX, offsetY + halfHeight, offsetZ);
 
@@ -202,6 +203,7 @@ public:
 			glVertex3f(x2, offsetY + halfHeight, z2);
 
 			// ---- Lower Circle ----
+			glColor3f(1.0f, (i % 2 == 0 ? 0.5f : 1.0f), 0.5f);
 			//Center of the circle
 			glVertex3f(offsetX, offsetY - halfHeight, offsetZ);
 
@@ -212,6 +214,7 @@ public:
 			glVertex3f(x2, offsetY - halfHeight, z2);
 
 			// ---- Upper Triangle ----
+			glColor3f(0.5f, 1.0f, 0.5f);
 			//First point
 			glVertex3f(x1, offsetY + halfHeight, z1);
 
@@ -222,6 +225,7 @@ public:
 			glVertex3f(x1, offsetY - halfHeight, z1);
 
 			// ---- Lower Triangle ----
+			glColor3f(0.5f, 0.5f, 1.0f);
 			//First point
 			glVertex3f(x1, offsetY - halfHeight, z1);
 
@@ -257,6 +261,7 @@ public:
 			float z2 = radius * sin(((i + 1) * t) * PI / 180.0);
 
 			// ---- Circle ----
+			glColor3f(1.0f, (i % 2 == 0 ? 0.5f : 1.0f), 0.5f);
 			//Center of the circle
 			glVertex3f(offsetX, offsetY - halfHeight, offsetZ);
 
@@ -267,6 +272,7 @@ public:
 			glVertex3f(x2, offsetY - halfHeight, z2);
 
 			// ---- Triangle ----
+			glColor3f(0.5f, (i % 2 == 0 ? 0.5f : 1.0f), (i % 2 == 0 ? 1.0f : 0.5f));
 			//Upper vertex
 			glVertex3f(offsetX, offsetY + halfHeight, offsetZ);
 
@@ -280,18 +286,46 @@ public:
 		glEnd();										// Finished Drawing The Triangles
 	}
 
+	void wavingOrbiter(const Matrix& viewMatrix, float rot1, float rot2, float amplitude, float l1, float l2)
+	{
+		//Matrix Transformation
+		Matrix translate1 = Matrix::makeTranslationMatrix(Vector(0.0f, amplitude * sin(rot1 * PI / 180.0f), 0.0f));
+		//Matrix rotate1 = Matrix::makeRotateMatrix(rot2, Vector(0.0f, 1.0f, 0.0f));
+		Matrix translate2 = Matrix::makeTranslationMatrix(Vector(l1 * cos(rot2 * PI / 180.0f), 0.0f, l2 * sin(rot2 * PI / 180.0f)));
+		//Matrix scale1 = Matrix::makeScaleMatrix(Vector(2.0f, 1.0f, 3.0f));
+
+		// NOTE on OpenGL Matrix model
+		// Screen = Proj * View * Model
+		// Model = TransformC(3rd) * TransformB(2nd) * TransformA(1st) (Transform could be Rotate, Scale, Translate, etc)
+
+		//Perform Model Transformation
+		Matrix modelMatrix = translate1 * translate2;
+
+		Matrix viewSpaceMatrix = viewMatrix * modelMatrix;
+
+		glLoadMatrixf((GLfloat*)viewSpaceMatrix.mVal);
+	}
+
+	float rot1 = 0.0f;
+	float rot2 = 0.0f;
+
 	void draw(const Matrix& viewMatrix)
 	{
 		drawAxis(viewMatrix);
 
 		glLoadMatrixf((GLfloat*)viewMatrix.mVal);
 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Show Wireframes
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Show Wireframes
 
-		//drawCube();
+		rot1 += 0.01f;
+		rot2 += 0.05f;
+
+		wavingOrbiter(viewMatrix, rot2, rot1, 2.0f, 10.0f, 5.0f);
+
+		drawCube();
 		//drawPyramid();
 
-		drawCylinder(2.0f, 1.0f);
+		//drawCylinder(2.0f, 1.0f);
 		//drawCone(2.0f, 1.0f);
 	}
 };
